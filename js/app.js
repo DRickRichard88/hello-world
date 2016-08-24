a00984af87b1e345a48b1e6764f5eb16097d3fd7
DRickRichard88 <kubabielecki88@gmail.com>
2016-08-24 15:42:37 +0200

    Add files via upload
    Wydaje mi się że "wydajniej" bym to zrobił może jakąś pętlą muszę pomyśleć hmm if ? To jedyne co mi przychodzi do głowy :) będę wdzięczny za ocenę mojej pracy :)

---
+++ js/app.js
@@ -0,0 +1,36 @@
+function myFunction() {
+	document.getElementById("img-section-one").src='./photos/photo1.png';
+	};
+	function myFunctionOne() {
+		document.getElementById("img-section-one").src='./photos/photo2.png';
+	};
+	function myFunctionTwo() {
+		document.getElementById("img-section-one").src='./photos/photo3.png';
+	};
+	function myFunctionThree() {
+		document.getElementById("img-section-one").src='./photos/minimo1.png';
+	};
+
+if (time < 10) {
+    greeting = "Good morning";
+} else if (time < 20) {
+    greeting = "Good day";
+} else {
+    greeting = "Good evening";
+}
+// function myFunction() {
+//     var x = document.getElementById("img-section-one").src;
+//     document.getElementById("img-section-one").innerHTML = x;
+// }
+
+
+// var x = document.getElementById("'img-section-one");
+
+// onclick="document.getElementById('img-section-
+// one').src='./photos/photo1.png'"
+
+// function myFunction() {
+//     var x = document.createElement("IMG");
+//     x.setAttribute("src", "img_pulpit.jpg");
+//     document.body.appendChild(x);
+// }
